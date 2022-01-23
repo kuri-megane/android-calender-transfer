f40ef6e33a3f84cd19e66107f5fdfe9193004845
kuri.megane my_ubuntu <kuri.megane3060.4@gmail.com>
2022-01-23 20:48:09 +0900

    [add] MainActivityを追加

---
+++ app/src/main/java/io/github/kuri_megane/android_calender_transfer/MainActivity.kt
@@ -0,0 +1,11 @@
+package io.github.kuri_megane.android_calender_transfer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
